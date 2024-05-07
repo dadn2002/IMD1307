f34377536d68a65146d5e824534688c19ae872d9
dadn2002 <nmdavialves@gmail.com>
2024-05-07 15:38:28 -0300

    5

---
+++ script.js
@@ -0,0 +1,24 @@
+// JavaScript for making the button draggable
+const button = document.getElementById('draggable-button');
+let isDragging = false;
+
+button.addEventListener('mousedown', startDrag);
+button.addEventListener('mousemove', drag);
+button.addEventListener('mouseup', endDrag);
+
+function startDrag(e) {
+    isDragging = true;
+    offsetX = e.clientX - button.getBoundingClientRect().left;
+    offsetY = e.clientY - button.getBoundingClientRect().top;
+}
+
+function drag(e) {
+    if (isDragging) {
+        button.style.left = (e.clientX - offsetX) + 'px';
+        button.style.top = (e.clientY - offsetY) + 'px';
+    }
+}
+
+function endDrag() {
+    isDragging = false;
+}
