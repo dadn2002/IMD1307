8f28dd874f982c7783f36f1853b1a8d64f11bf8f
dadn2002 <nmdavialves@gmail.com>
2024-05-07 15:50:22 -0300

    11

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // JavaScript for making the button draggable
 const button = document.getElementById('draggable-button');
 let isDragging = false;
-let offsetX, offsetY;
+let initialX, initialY;
 
 button.addEventListener('mousedown', startDrag);
 document.addEventListener('mousemove', drag);
@@ -9,18 +9,21 @@ document.addEventListener('mouseup', endDrag);
 
 function startDrag(e) {
     isDragging = true;
-    const rect = button.getBoundingClientRect();
-    offsetX = e.clientX - rect.left;
-    offsetY = e.clientY - rect.top;
+    initialX = e.clientX;
+    initialY = e.clientY;
 }
 
 function drag(e) {
     if (isDragging) {
-        const mapRect = document.querySelector('.map-container').getBoundingClientRect();
-        const newX = e.clientX - offsetX - mapRect.left;
-        const newY = e.clientY - offsetY - mapRect.top;
+        const deltaX = e.clientX - initialX;
+        const deltaY = e.clientY - initialY;
+        const rect = button.getBoundingClientRect();
+        const newX = rect.left + deltaX;
+        const newY = rect.top + deltaY;
         button.style.left = newX + 'px';
         button.style.top = newY + 'px';
+        initialX = e.clientX;
+        initialY = e.clientY;
     }
 }
 
