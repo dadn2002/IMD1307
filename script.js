7b820127ddaf73448bb4c709426754048ba7cbfd
dadn2002 <nmdavialves@gmail.com>
2024-05-07 15:51:51 -0300

    12

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// JavaScript for making the button draggable
-const button = document.getElementById('draggable-button');
+// JavaScript for making the sphere draggable
+const sphere = document.getElementById('draggable-sphere');
 let isDragging = false;
 let initialX, initialY;
 
-button.addEventListener('mousedown', startDrag);
+sphere.addEventListener('mousedown', startDrag);
 document.addEventListener('mousemove', drag);
 document.addEventListener('mouseup', endDrag);
 
@@ -17,11 +17,11 @@ function drag(e) {
     if (isDragging) {
         const deltaX = e.clientX - initialX;
         const deltaY = e.clientY - initialY;
-        const rect = button.getBoundingClientRect();
+        const rect = sphere.getBoundingClientRect();
         const newX = rect.left + deltaX;
         const newY = rect.top + deltaY;
-        button.style.left = newX + 'px';
-        button.style.top = newY + 'px';
+        sphere.style.left = newX + 'px';
+        sphere.style.top = newY + 'px';
         initialX = e.clientX;
         initialY = e.clientY;
     }
